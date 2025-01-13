c2048571aa3732d8b346660fe8c6e4987fee8d21
Hareesh292 <vharishv959@gmail.com>
2025-01-13 12:54:48 +0530

    find the tech number is done

--- vikramm.py
+++ vikramm.py
@@ -1,1 +1,12 @@
-print('hareesh')
+num=2520
+ss=str(num)
+if len(str(num))%2==0:
+    fh=int(ss[len(ss)//2:])
+    sh=int(ss[:len(ss)//2])
+    if (fh+sh)**2==num:
+        print('tech number')
+    else:
+        print('not tech number')
+else:
+    print('not tech number')
+
